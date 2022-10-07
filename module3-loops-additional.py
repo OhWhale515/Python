9387defb1f571e2e1f113c67049cb87258cdeda8
Sterling <sterlinggreen515@gmail.com>
2022-10-07 19:37:01 +0000

    new modules

---
+++ module3-loops-additional.py
@@ -0,0 +1,12 @@
+for a in range(1,6):
+    for b in range(1, 6):
+        print(a, 'x', b, '=', a * b)
+        
+print()
+
+i = 1
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print('else:', i)
---
+++ module4-intro-lists.py
@@ -0,0 +1,16 @@
+#-lists
+empty_list = []
+top_cities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+print(top_cities)
+
+#-list indexing
+top_cities = [0]
+top_cities = [3]
+
+#-list slicing
+top_cities[:]
+top_cities[0:]
+top_cities[:4]
+
+# tuples
+# dictionaries
