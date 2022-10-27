ea006cbd925cbd86a97cbd2700ffdbfb500f6351
OhWhale515 <sterlinggreen515@gmail.com>
2022-10-27 16:18:13 -0400

    lambdapythonupdate

---
+++ Python_Temps/boto3_lambda_pythonproject.py
@@ -0,0 +1,16 @@
+import json
+import boto3
+def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+
+    if event:
+        print("Event : ", event)
+        file_obj = event["Records"][0]
+        filename = str(file_obj[s3]['object']['key'])
+        print("Filename: ", filename)
+        fileObj = s3.get_object(Bucket = "aws-lambda-trigger", Key=filename)
+        file_content = fileObj["Body"].read().decode('utf-8')
+        print(file_content)
+
+
+    return "Thank you bruh"
