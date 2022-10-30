d0c8a224ff897dcaa280e9cac63fdd1ec44b597f
OhWhale515 <sterlinggreen515@gmail.com>
2022-10-30 17:36:12 -0400

    word placement script

---
+++ Python_Temps/SideProjects/Word_Placement.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str = "Yoo u really coding bro lol."
+    word_to_replace = input("u: ")
+    word_replacement = input("you: ")
+    print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
