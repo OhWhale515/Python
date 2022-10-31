4efacfdcbfb3f0969554289a498f236a018355f5
OhWhale515 <sterlinggreen515@gmail.com>
2022-10-30 21:52:45 -0400

    Dice roller syntax updated

--- Python_Temps/SideProjects/DiceRoller.py
+++ Python_Temps/SideProjects/DiceRoller.py
@@ -1,58 +1,62 @@
 import random
 
 def roll_dice():
-     1: (
-            "-----",
-            "|   |",
-            "| o |",
-            "|   |",
-            "-----",
-        ),
-        2: (
-            "-----",
-            "|o  |",
-            "|   |",
-            "|  o|",
-            "-----",
-        ),
-        3: (
-            "-----",
-            "|o  |",
-            "| o |",
-            "|  o|",
-            "-----",
-        ),
-        4: (
-            "-----",
-            "|o o|",
-            "|   |",
-            "|o o|",
-            "-----",
-        ),
-        5: (
-            "-----",
-            "|o o|",
-            "| o |",
-            "|o o|",
-            "-----",
-        ),
-        6: (
-            "-----",
-            "|o o|",
-            "|o o|",
-            "|o o|",
-            "-----",
+
+    dice_drawing = {
+        1: (
+                "-----",
+                "|   |",
+                "| o |",
+                "|   |",
+                "-----",
+            ),
+            2: (
+                "-----",
+                "|o  |",
+                "|   |",
+                "|  o|",
+                "-----",
+            ),
+            3: (
+                "-----",
+                "|o  |",
+                "| o |",
+                "|  o|",
+                "-----",
+            ),
+            4: (
+                "-----",
+                "|o o|",
+                "|   |",
+                "|o o|",
+                "-----",
+            ),
+            5: (
+                "-----",
+                "|o o|",
+                "| o |",
+                "|o o|",
+                "-----",
+            ),
+            6: (
+                "-----",
+                "|o o|",
+                "|o o|",
+                "|o o|",
+                "-----",
         ),
 
     }
 
     roll = input("U shootin fam? (Shoot/Pass): ")
 
-    while roll.lower() == "Yes".lower():
+    while roll.lower() == "Shoot".lower():
         dice1 = random.randint(1, 6)
         dice2 = random.randint(1, 6)
 
-        print("dice rolled: {} and {)".format(dice1, dice2))
+        print("dice rolled: {} and {}". format(dice1, dice2))
+        print("\n".join(dice_drawing[dice1]))
+        print("\n".join(dice_drawing[dice2]))
 
         roll = input("U shootin again? (Shoot/Pass): ")
 
