909610696a981cd5ab2a0c9e62ea4203a7577d9f
OhWhale515 <sterlinggreen515@gmail.com>
2022-11-02 17:39:54 -0400

    Updated lambda code. can anem 1 bucket now thru api gateway

--- Python_Temps/boto3_lambda_pythonproject.py
+++ Python_Temps/boto3_lambda_pythonproject.py
@@ -4,16 +4,19 @@ import boto3
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
     # TODO implement
-    print('Hello from lambda!')
+    print('Here Is The s3 buckets!')
     
     response = s3.list_buckets()
-    
+
     bucket_names = []
     buckets = response["Buckets"]
     
     for bucket in buckets:
         print(bucket["Name"])
         bucket_names.append(bucket["Name"])
+    
+    
     return {
-        'body': json.dumps('bucket_names')
+        'body': json.dumps(bucket["Name"])
+        
     }
