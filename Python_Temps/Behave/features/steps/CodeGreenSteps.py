01c3df1b5be052d8b0ac3fd8c2de5c3ab0c7efa1
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-05 13:34:50 -0500

    removed raise area to replace

--- Python_Temps/Behave/features/steps/CodeGreenSteps.py
+++ Python_Temps/Behave/features/steps/CodeGreenSteps.py
@@ -3,19 +3,19 @@ from selenium import webdriver
 
 @given(u'launch chrome browser')
 def launchBrowser(context):
-    raise NotImplementedError(u'STEP: Given launch chrome browser')
+    
 
 
 @when(u'open CodeGreen home page')
 def openHomepage(context):
-    raise NotImplementedError(u'STEP: When open CodeGreen home page')
+    
 
 
 @then(u'verify that the Logo present on page')
 def verifyLogo(context):
-    raise NotImplementedError(u'STEP: Then verify that the Logo present on page')
+    
 
 
 @then(u'close browser')
 def closeBrowser(context):
-    raise NotImplementedError(u'STEP: Then close browser')
+    
