10ad06973f6d8c9ef82146a09b302819e6c73799
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-05 13:33:26 -0500

    changed step_impl to desired action names

--- Python_Temps/Behave/features/steps/CodeGreenSteps.py
+++ Python_Temps/Behave/features/steps/CodeGreenSteps.py
@@ -2,20 +2,20 @@ from behave import *
 from selenium import webdriver
 
 @given(u'launch chrome browser')
-def step_impl(context):
+def launchBrowser(context):
     raise NotImplementedError(u'STEP: Given launch chrome browser')
 
 
 @when(u'open CodeGreen home page')
-def step_impl(context):
+def openHomepage(context):
     raise NotImplementedError(u'STEP: When open CodeGreen home page')
 
 
 @then(u'verify that the Logo present on page')
-def step_impl(context):
+def verifyLogo(context):
     raise NotImplementedError(u'STEP: Then verify that the Logo present on page')
 
 
 @then(u'close browser')
-def step_impl(context):
+def closeBrowser(context):
     raise NotImplementedError(u'STEP: Then close browser')
