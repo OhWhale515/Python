14e6d907e17aaa3938782fa87d3be2c1fdd72c8b
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-06 16:30:09 -0500

    practiced w/ turtle, created table w/ prettytable

---
+++ Python_Temps/SideProjects/TurtleDemo/main.py
@@ -0,0 +1,22 @@
+# import turtles
+# print(turtles.another_variable)
+
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DarkGrey")
+# timmy.forward(100)
+
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+
+
+# print("| Pokemon Name | Type |")
+# print("------------------------")
+
