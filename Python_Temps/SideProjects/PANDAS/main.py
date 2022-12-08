267b739b74c6ac6d6b8baf997180921b746f5d01
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-08 14:02:01 -0500

    added weather averages for degrees

--- Python_Temps/SideProjects/PANDAS/main.py
+++ Python_Temps/SideProjects/PANDAS/main.py
@@ -12,8 +12,17 @@
 #             temperatures.append(int(row[1]))
 #         print(temperatures)
 
-
 import pandas
-data = pandas.read_csv("weather_data.csv")
-print(data)
 
+data = pandas.read_csv("weather_data.csv")
+# print(data)
+# print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(len(temp_list))
+
+average = sum(temp_list) / len(temp_list)
+print(average)
