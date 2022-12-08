de05d3fb7069999fc1c47dd5b37c421654b380ab
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-08 14:13:34 -0500

    add strings to print chart rows and columns

--- Python_Temps/SideProjects/PANDAS/main.py
+++ Python_Temps/SideProjects/PANDAS/main.py
@@ -18,11 +18,21 @@ data = pandas.read_csv("weather_data.csv")
 # print(data)
 # print(data["temp"])
 
-data_dict = data.to_dict()
-print(data_dict)
+# data_dict = data.to_dict()
+# print(data_dict)
 
-temp_list = data["temp"].to_list()
-print(len(temp_list))
+# temp_list = data["temp"].to_list()
+# print(len(temp_list))
+# print(data["temp"].mean())
+# print(data["temp"].max())
+# # average = sum(temp_list) / len(temp_list)
+# # print(average)
 
-average = sum(temp_list) / len(temp_list)
-print(average)
+
+# # Data Columns!
+# # print(data["condition"])
+# print(data.condition)
+
+# Data Rows!
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
