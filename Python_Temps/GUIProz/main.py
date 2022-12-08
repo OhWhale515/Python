f785645ab1d572ed8dea7c4baba3b868657a3c6c
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-08 18:45:40 -0500

    Tkinter GUI CGP

---
+++ Python_Temps/GUIProz/main.py
@@ -0,0 +1,12 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("CodeGreen Project")
+window.minsize(width=500, height=300)
+
+
+#Label
+
+my_label = tkinter.Label(text="CodeGreen Project")
+
+window.mainloop()
