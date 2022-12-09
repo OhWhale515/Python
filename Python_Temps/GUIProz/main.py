f140b1df070e344ea1085cd5147f5fd04c2f0ca7
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-09 06:33:30 -0500

    GUI set up with tkinter and turtle imports

--- Python_Temps/GUIProz/main.py
+++ Python_Temps/GUIProz/main.py
@@ -7,6 +7,11 @@ window.minsize(width=500, height=300)
 
 #Label
 
-my_label = tkinter.Label(text="CodeGreen Project")
+my_label = tkinter.Label(text="CodeGreen Projectz", font=("Arial", 22, "bold"))
+my_label.pack(side="left")
 
+# import turtle
+
+# tim = turtle.Turtle()
+# tim.write("Some Text", font="Times New Roman" 30)
 window.mainloop()
