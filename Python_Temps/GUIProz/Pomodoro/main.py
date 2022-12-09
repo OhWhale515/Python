c973cd4e623bb78ee45534420594a9099f2fe8f3
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-09 14:27:14 -0500

    updated GUI colors and added start/stop

--- Python_Temps/GUIProz/Pomodoro/main.py
+++ Python_Temps/GUIProz/Pomodoro/main.py
@@ -18,16 +18,24 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+# fg=GREEN
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=tomato_img)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start")
+stop_button = Button(text="Stop")
 
 
-canvas.pack()
+
+# canvas.pack()
 
 
 
