b3ae15bbd0d9b496b72495065dccdb18ff385389
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-09 12:11:35 -0500

    added click here button and header

--- Python_Temps/GUIProz/main.py
+++ Python_Temps/GUIProz/main.py
@@ -1,14 +1,36 @@
 import tkinter
+from tkinter import 
+
+def button_clicked():
+    print ("Button clicked!")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
 
 window = tkinter.Tk()
 window.title("CodeGreen Project")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 
 
 #Label
+my_label = Label(text="CodeGreen Projectz", font=("Arial", 22, "bold"))
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
+# my_label.pack(side="left")
 
-my_label = tkinter.Label(text="CodeGreen Projectz", font=("Arial", 22, "bold"))
-my_label.pack(side="left")
+# Button
+button = Button(text="Click Here", command=button_clicked)
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+
+# Entry
+input = Entry(width=10)
 
 # import turtle
 
